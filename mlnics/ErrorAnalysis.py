Added min-max normalization, small fixes

mlnics/ErrorAnalysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fenics import *
 from rbnics import *
+from rbnics.utils.io.text_line import TextLine
 from mlnics.Normalization import IdentityNormalization
 from mlnics.NN import RONN
 
@@ -135,9 +136,11 @@ def error_analysis_fixed_net(ronn, mu, input_normalization, output_normalization
     if print_results:
         if len(ronn.problem.components) > 1:
             raise NotImplementedError("Error analysis for multi-component problems not implemented yet.")
-        print("Mean Relative Error:")
-        print("N\tNN-HF\t\t\tNN-RO\t\t\tRO-HF")
-        print(f"{ronn.ro_dim}\t{np.mean(nn_hf_error)}\t{np.mean(nn_ro_error)}\t{np.mean(ro_hf_error)}")
+        print(TextLine("N = "+str(ronn.ro_dim), fill="#"))
+        print(f"ERROR\tNN-HF\t\t\tNN-RO\t\t\tRO-HF")
+        print(f"min\t{np.min(nn_hf_error)}\t{np.min(nn_ro_error)}\t{np.min(ro_hf_error)}")
+        print(f"mean\t{np.mean(nn_hf_error)}\t{np.mean(nn_ro_error)}\t{np.mean(ro_hf_error)}")
+        print(f"max\t{np.max(nn_hf_error)}\t{np.max(nn_ro_error)}\t{np.max(ro_hf_error)}")
 
     # perhaps return the solutions too so that they don't have to be computed every time this is called
     return nn_hf_error, nn_ro_error, ro_hf_error

mlnics/Normalization.py
@@ -47,9 +47,27 @@ class StandardNormalization(Normalization):
         if normalize:
             if self.mean is None and self.std is None:
                 self.initialized = True
-                self.mean = torch.mean(matrix, axis=self.axis, keepdims=True)
-                self.std = torch.std(matrix, axis=self.axis, keepdims=True)
-                self.std[torch.abs(self.std) <= 1e-6] = 1.
+                # self.mean = torch.mean(matrix, axis=self.axis, keepdims=True)
+                # self.std = torch.std(matrix, axis=self.axis, keepdims=True)
+                # self.std[torch.abs(self.std) <= 1e-6] = 1.
+                self.mean = torch.mean(matrix)
+                self.std = torch.std(matrix)
             return (matrix - self.mean) / self.std
         else:
             return matrix * self.std + self.mean
+
+class MinMaxNormalization(Normalization):
+     def __init__(self, input_normalization=False):
+         super(MinMaxNormalization, self).__init__(input_normalization)
+         self.min = None
+         self.max = None
+
+     def __call__(self, matrix, normalize=True):
+         if normalize:
+             if self.min is None and self.max is None:
+                 self.initialized = True
+                 self.min = torch.min(matrix, axis=self.axis, keepdims=True)
+                 self.max = torch.max(matrix, axis=self.axis, keepdims=True)
+             return (matrix - self.min) / (self.max - self.min)
+         else:
+             return (self.max - self.min) * matrix + self.min