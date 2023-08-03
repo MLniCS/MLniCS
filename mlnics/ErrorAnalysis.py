29672a1bf35a55b660d2cc3539bdffe5945a6217
Max Hirsch <mhirsch@berkeley.edu>
2023-08-03 14:48:38 -0400

    Add Federico's changes

--- mlnics/ErrorAnalysis.py
+++ mlnics/ErrorAnalysis.py
@@ -12,8 +12,14 @@ NN_FOLDER = "/nn_results"
 
 def compute_reduced_solutions(reduced_problem, mu):
     """
-    Compute the high fidelity solutions for each value of mu.
-    mu: torch.tensor
+    Computes the high-fidelity solutions for each value of `mu`.
+    
+    Parameters:
+        reduced_problem (Object): The reduced problem instance.
+        mu (torch.tensor): A tensor containing the values of `mu` for which solutions need to be computed.
+    
+    Returns:
+        numpy.ndarray: A numpy array containing the solutions.
     """
     solutions = []
 
@@ -43,9 +49,20 @@ def compute_reduced_solutions(reduced_problem, mu):
 
 def compute_error(ronn, pred, ro_solutions, euclidean=False, relative=True, eps=1e-12):
     """
-    Compute error based on the inner product for the given problem.
-    Assumes that input normalization has already been applied to normalized_mu.
-    hf_solutions are the high fidelity solutions corresponding to the non-normalized mu.
+    Compute the error between the predicted and actual high-fidelity solutions.
+    It assumes that input normalization has already been applied to the normalized mu.
+    The error can be calculated in either relative or absolute form, and can be computed using the Euclidean norm or the inner product.
+
+    Parameters:
+        ronn (object): The reduced order model object.
+        pred (np.ndarray): The predicted high-fidelity solutions.
+        ro_solutions (np.ndarray): The actual high-fidelity solutions.
+        euclidean (bool, optional): If True, the error will be computed using the Euclidean norm, otherwise it will be computed using the inner product. Default is False.
+        relative (bool, optional): If True, the error will be computed in relative form, otherwise it will be computed in absolute form. Default is True.
+        eps (float, optional): A small number used to avoid division by zero in the relative error calculation. Default is 1e-12.
+
+    Returns:
+        list: A list of errors, one for each prediction.
     """
 
     errors = []
@@ -82,7 +99,29 @@ def compute_error(ronn, pred, ro_solutions, euclidean=False, relative=True, eps=
 
 def plot_error(ronn, data, mu, input_normalization=None, output_normalization=None, ind1=0, ind2=1, cmap="bwr"):
     """
-    mu is not normalized and not time augmented
+    Plots the error of the reduced order neural network (ronn) predictions using data, mu and a specified color map.
+
+    :param ronn: The reduced order neural network model.
+    :type ronn: torch.nn.Module
+    :param data: The data used for the model.
+    :type data: object
+    :param mu: The parameters for the ronn model.
+    :type mu: np.ndarray
+    :param input_normalization: The normalization method for the input data. Defaults to None.
+    :type input_normalization: callable or None, optional
+    :param output_normalization: The normalization method for the output data. Defaults to None.
+    :type output_normalization: callable or None, optional
+    :param ind1: The index of the first parameter to be plotted. Defaults to 0.
+    :type ind1: int, optional
+    :param ind2: The index of the second parameter to be plotted. Defaults to 1.
+    :type ind2: int, optional
+    :param cmap: The color map to be used for plotting. Defaults to "bwr".
+    :type cmap: str, optional
+    :return: The computed errors and the plot.
+    :rtype: tuple (np.ndarray, matplotlib.collections.PathCollection)
+
+    Note:
+    mu is not normalized and not time augmented.
     """
     if input_normalization is None:
         input_normalization = IdentityNormalization()
@@ -110,7 +149,41 @@ def get_residuals(ronn, data, mu,
                   loss_functions=None,
                   plot_residuals=True, ind1=0, ind2=1, cmap="bwr"):
     """
-    mu is not normalized and not time augmented
+    Calculate the residuals of a reduced order neural network (RONN) based on input data and parameters `mu`.
+
+    Parameters:
+    ronn : reduced order neural network model
+    The reduced order neural network model to be evaluated.
+    data : data object
+    The data object that contains the training data.
+    mu : numpy array
+    The parameters to be evaluated.
+    input_normalization : callable or None, optional (default: None)
+    The normalization function for the input parameters.
+    If None, the `IdentityNormalization` function will be used.
+    output_normalization : callable or None, optional (default: None)
+    The normalization function for the output data.
+    loss_functions : list or None, optional (default: None)
+    The list of loss functions to be used for evaluating the RONN.
+    plot_residuals : bool, optional (default: True)
+    Whether to plot the residuals and save the plot to disk.
+    ind1 : int, optional (default: 0)
+    The first index of the parameter array `mu` to be used in plotting.
+    ind2 : int, optional (default: 1)
+    The second index of the parameter array `mu` to be used in plotting.
+    cmap : str, optional (default: "bwr")
+    The color map to be used in plotting the residuals.
+
+    Returns:
+    losses : numpy array
+    The array of residuals.
+    plot : matplotlib.collections.PathCollection or None
+    The plot of the residuals, if `plot_residuals` is True.
+    loss_functions : list
+    The list of loss functions used for evaluating the RONN.
+
+    Notes
+    - `mu` is not normalized and not time augmented.
     """
 
     if input_normalization is None:
@@ -149,7 +222,19 @@ def get_residuals(ronn, data, mu,
 
 def error_analysis_fixed_net(ronn, mu, input_normalization, output_normalization, euclidean=False, relative=True, print_results=True):
     """
-    mu: calculate error of neural network ronn on test set mu (not time augmented)
+    Analyze the error of a neural network on test set `mu` (not time augmented).
+
+    Parameters:
+    - `ronn` (object): A neural network object.
+    - `mu` (array-like): Test set.
+    - `input_normalization` (function): Function to normalize input data.
+    - `output_normalization` (function): Function to normalize output data.
+    - `euclidean` (bool, optional): Use euclidean distance for error computation. Defaults to False.
+    - `relative` (bool, optional): Use relative error for computation. Defaults to True.
+    - `print_results` (bool, optional): Print results to console. Defaults to True.
+
+    Returns:
+    - tuple of arrays: Three arrays containing errors of neural network compared to high-fidelity, neural network compared to reduced order, and reduced order compared to high-fidelity, respectively.
     """
 
     # get neural network predictions
@@ -189,7 +274,17 @@ def error_analysis_fixed_net(ronn, mu, input_normalization, output_normalization
 
 def error_analysis_by_network(nets, mu, input_normalizations, output_normalizations, euclidean=False, relative=True):
     """
-    nets: dictionary of neural networks
+    This function calculates the error of the neural network `ronn` on the test set `mu` (not time augmented). 
+        
+    :param ronn: The neural network to be evaluated.
+    :param mu: The test set to be used for error evaluation.
+    :param input_normalization: Normalization function applied to the input data.
+    :param output_normalization: Normalization function applied to the output data.
+    :param euclidean: Boolean indicating whether to use euclidean error calculation. Defaults to False.
+    :param relative: Boolean indicating whether to use relative error calculation. Defaults to True.
+    :param print_results: Boolean indicating whether to print the results. Defaults to True.
+    :return: Tuple containing the errors between (1) neural network and high-fidelity, (2) neural network and reduced-order, and (3) reduced-order and high-fidelity.
+    :rtype: Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]
     """
 
     print(85*"#")
@@ -234,11 +329,64 @@ def error_analysis(ronn, mu, input_normalization, n_hidden=2, n_neurons=100, act
 
     raise NotImplementedError()
 
+def plot_solution(ronn, mu, input_normalization=None, output_normalization=None, t=-1, colorbar=True, component=-1):
+    """
+    Plots the solution of a reduced order neural network model.
+
+    Parameters:
+    ronn (object): An instance of the reduced order neural network class.
+    mu (tuple): A tuple representing the parameter values for the model.
+    input_normalization (None, optional): The normalization applied to the inputs. Defaults to None.
+    output_normalization (None, optional): The normalization applied to the outputs. Defaults to None.
+    t (int, optional): The time step for a time dependent model. Defaults to 0.
+    colorbar (bool, optional): Flag to indicate if a colorbar should be displayed with the plot. Defaults to True.
+    component (int, optional): The component number to plot. Defaults to -1.
+
+    Returns:
+    None
+    """
+    problem = ronn.problem
+
+    problem.set_mu(mu)
+    problem.solve()
+
+    if not ronn.time_dependent:
+        if component != -1:
+            P = plot(problem._solution, component=component)
+        else:
+            P = plot(problem._solution)
+    else:
+        if component != -1:
+            P = plot(problem._solution_over_time[t], component=component)
+        else:
+            P = plot(problem._solution_over_time[t])
+
+    if colorbar:
+        plt.colorbar(P)
+
+    plt.title("Solution field at $\mu$ = "+str(tuple(round(i, 2) for i in mu)))
+    folder = ronn.reduction_method.folder_prefix + NN_FOLDER + "/" + ronn.name()
+    plt.savefig(folder + "/" + ronn.name() + f"_{mu}_solution.png")
+
 def plot_solution_difference(ronn, mu, input_normalization=None, output_normalization=None, t=0, colorbar=False, component=-1):
     """
-    mu is a tuple.
-    t is an int.
-    component is a str.
+    Plot the difference between the solution of the original problem and the reduced neural network solution.
+    
+    Parameters:
+    ronn : object
+    Instance of the reduced neural network.
+    mu : tuple
+    The parameter of the PDE.
+    input_normalization : float, optional
+    Normalization factor for the input, by default None
+    output_normalization : float, optional
+    Normalization factor for the output, by default None
+    t : int, optional
+    Time step for time-dependent problems, by default 0
+    colorbar : bool, optional
+    Show colorbar, by default True
+    component : int, optional
+    The component of the solution to be plotted, by default -1 (all components)
     """
     problem = ronn.problem
     reduced_problem = ronn.reduced_problem
