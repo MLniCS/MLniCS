d4cdb7c9448bac590ecc8908f5feac238fcaf608
Max Hirsch <mhirsch@berkeley.edu>
2023-08-03 17:11:18 -0400

    add Federico's class descriptions

--- mlnics/Normalization.py
+++ mlnics/Normalization.py
@@ -5,6 +5,17 @@ Normalization functions to operate on snapshot matrix
 """
 
 class Normalization:
+    """
+    Abstract class for normalization operations in matrix data.
+
+    Attributes:
+    initialized (bool): Whether the normalization parameters have been initialized.
+    axis (int): Axis to perform normalization along, either 0 for input normalization or 1 for channel normalization.
+
+    Methods:
+    call (matrix, normalize=True): Raises a NotImplementedError, must be overridden in subclass.
+    """
+
     def __init__(self, input_normalization=False):
         super(Normalization, self).__init__()
         self.initialized = False
@@ -17,6 +28,20 @@ class Normalization:
         raise NotImplementedError("Calling method of abstract class Normalization")
 
 class IdentityNormalization(Normalization):
+    """
+    Identity normalization, returns the input matrix unchanged.
+
+    Inherits from:
+    Normalization
+
+    Attributes:
+    initialized (bool): Whether the normalization parameters have been initialized.
+    axis (int): Axis to perform normalization along, either 0 for input normalization or 1 for channel normalization.
+
+    Methods:
+    call (matrix, normalize=True): Returns the input matrix, normalize argument has no effect.
+    """
+
     def __init__(self, input_normalization=False):
         super(IdentityNormalization, self).__init__(input_normalization)
         self.initialized = True
@@ -25,6 +50,21 @@ class IdentityNormalization(Normalization):
         return matrix
 
 class MeanNormalization(Normalization):
+    """
+    Mean normalization, subtracts the mean of the matrix along the specified axis.
+
+    Inherits from:
+    Normalization
+
+    Attributes:
+    initialized (bool): Whether the normalization parameters have been initialized.
+    axis (int): Axis to perform normalization along, either 0 for input normalization or 1 for channel normalization.
+    mean (torch.Tensor): Mean of the matrix along the specified axis, None if not yet initialized.
+
+    Methods:
+    call (matrix, normalize=True): Returns the input matrix with the mean subtracted if normalize is True, or adds the mean if normalize is False.
+    """
+
     def __init__(self, input_normalization=False):
         super(MeanNormalization, self).__init__(input_normalization)
         self.mean = None
@@ -39,6 +79,22 @@ class MeanNormalization(Normalization):
             return matrix + self.mean
 
 class StandardNormalization(Normalization):
+    """
+    Standard normalization, subtracts the mean and divides by the standard deviation of the matrix along the specified axis.
+
+    Inherits from:
+    Normalization
+
+    Attributes:
+    initialized (bool): Whether the normalization parameters have been initialized.
+    axis (int): Axis to perform normalization along, either 0 for input normalization or 1 for channel normalization.
+    mean (torch.Tensor): Mean of the matrix along the specified axis, None if not yet initialized.
+    std (torch.Tensor): Standard deviation of the matrix along the specified axis, None if not yet initialized.
+
+    Methods:
+    call (matrix, normalize=True): Returns the input matrix with the mean subtracted and divided by the standard deviation if normalize is True, or multiplies by the standard deviation and adds the mean if normalize is False.
+    """
+
     def __init__(self, input_normalization=False):
         super(StandardNormalization, self).__init__(input_normalization)
         self.mean = None
@@ -58,6 +114,22 @@ class StandardNormalization(Normalization):
             return matrix * self.std + self.mean
 
 class MinMaxNormalization(Normalization):
+    """
+    Min-Max normalization, normalizes the matrix to the range [-1, 1] along the specified axis.
+
+    Inherits from:
+    Normalization
+
+    Attributes:
+    initialized (bool): Whether the normalization parameters have been initialized.
+    axis (int): Axis to perform normalization along, either 0 for input normalization or 1 for channel normalization.
+    min (torch.Tensor): Minimum of the matrix along the specified axis, None if not yet initialized.
+    max (torch.Tensor): Maximum of the matrix along the specified axis, None if not yet initialized.
+
+    Methods:
+    call (matrix, normalize=True): Returns the input matrix normalized to the range [-1, 1] if normalize is True, or returns the original values if normalize is False.
+    """
+
     def __init__(self, input_normalization=False):
         super(MinMaxNormalization, self).__init__(input_normalization)
         self.min = None
